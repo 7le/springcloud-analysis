105a49596c6c0fd4c08d3f761c1b7c383c5a824b
7le <silk.heqian@gmail.com>
2018-03-24 22:52:54 +0800

    add Hystrix test 🐡

--- ad/src/main/java/com/cloud/ad/AdApplication.java
+++ ad/src/main/java/com/cloud/ad/AdApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author 7le
  */
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
 public class AdApplication {
 
 	public static void main(String[] args) {
--- ad/src/main/java/com/cloud/ad/service/TestService.java
+++ ad/src/main/java/com/cloud/ad/service/TestService.java
@@ -1,5 +1,6 @@
 package com.cloud.ad.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,12 @@ public class TestService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "error")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+        return restTemplate.getForObject("http://spring-cloud-statistic/hi?name="+name,String.class);
+    }
+
+    public String error(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
