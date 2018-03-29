5c921221b6f36e84f0cfb2050a3411a2331c0f41
7le <silk.heqian@gmail.com>
2018-03-30 00:46:34 +0800

    add failure node to remove automatically  🍁

--- ad/src/main/java/com/cloud/ad/controller/AdController.java
+++ ad/src/main/java/com/cloud/ad/controller/AdController.java
@@ -2,16 +2,17 @@ package com.cloud.ad.controller;
 
 import com.cloud.ad.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * AdController
  *
  * @author : 7le
  * @since v1.0.0
- *
  */
 @RestController
 public class AdController {
@@ -19,13 +20,28 @@ public class AdController {
     @Autowired
     TestService testService;
 
+    @Value("${7le}")
+    private String str;
+
     @RequestMapping("/status")
     public String health() {
         return "Winter is coming";
     }
 
     @RequestMapping(value = "/hi")
-    public String hi(@RequestParam String name){
+    public String hi(@RequestParam String name) {
         return testService.hiService(name);
+    }
+
+    /**
+     * 响应式test 使用undertow
+     */
+    @RequestMapping(value = "/")
+    public Mono<String> test() {
+        System.out.println(Thread.currentThread().getName());
+        return Mono.create(s -> {
+            System.out.println(Thread.currentThread().getName());
+            s.success(str);
+        });
     }
 }
