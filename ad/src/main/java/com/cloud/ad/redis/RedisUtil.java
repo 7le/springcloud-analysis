9d1e97d737cfe88069d8180d89995f20c2b8a1bb
7le <silk.heqian@gmail.com>
2018-07-20 23:05:22 +0800

    优化工具类 🐡

--- ad/src/main/java/com/cloud/ad/redis/RedisUtil.java
+++ ad/src/main/java/com/cloud/ad/redis/RedisUtil.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.io.Serializable;
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
- * redis 工具类
+ * redis cache 工具类
  *
  * @author 7le
  */
@@ -20,45 +21,72 @@ public class RedisUtil {
     @SuppressWarnings("rawtypes")
     @Autowired
     private RedisTemplate redisTemplate;
+
+
+    //=============================common============================
+
     /**
-     * 批量删除对应的value
+     * 指定缓存失效时间
      *
-     * @param keys
-     */
-    public void remove(final String... keys) {
-        for (String key : keys) {
-            remove(key);
-        }
-    }
-    /**
-     * 批量删除key
-     *
-     * @param pattern
-     */
-    public void removePattern(final String pattern) {
-        Set<Serializable> keys = redisTemplate.keys(pattern);
-        if (keys.size() > 0)
-            redisTemplate.delete(keys);
-    }
-    /**
-     * 删除对应的value
-     *
-     * @param key
-     */
-    public void remove(final String key) {
-        if (exists(key)) {
-            redisTemplate.delete(key);
-        }
-    }
-    /**
-     * 判断缓存中是否有对应的value
-     *
-     * @param key
+     * @param key  键
+     * @param time 时间(秒)
      * @return
      */
-    public boolean exists(final String key) {
-        return redisTemplate.hasKey(key);
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
+
+    /**
+     * 根据key 获取过期时间
+     *
+     * @param key 键 不能为null
+     * @return 时间(秒) 返回0代表为永久有效
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 判断key是否存在
+     *
+     * @param key 键
+     * @return true 存在 false不存在
+     */
+    public boolean exists(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 删除缓存
+     *
+     * @param key 可以传一个值 或多个
+     */
+    @SuppressWarnings("unchecked")
+    public void del(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                redisTemplate.delete(key[0]);
+            } else {
+                redisTemplate.delete(CollectionUtils.arrayToList(key));
+            }
+        }
+    }
+
+    //============================String=============================
+
     /**
      * 读取缓存
      *
@@ -71,6 +99,7 @@ public class RedisUtil {
         result = operations.get(key);
         return result;
     }
+
     /**
      * 写入缓存
      *
@@ -89,6 +118,7 @@ public class RedisUtil {
         }
         return result;
     }
+
     /**
      * 写入缓存
      *
@@ -109,4 +139,151 @@ public class RedisUtil {
         }
         return result;
     }
+
+    //============================Hash=============================
+
+    /**
+     * HashGet
+     *
+     * @param key  键 不能为null
+     * @param item 项 不能为null
+     * @return 值
+     */
+    public Object hget(String key, String item) {
+        return redisTemplate.opsForHash().get(key, item);
+    }
+
+    /**
+     * 获取hashKey对应的所有键值
+     *
+     * @param key 键
+     * @return 对应的多个键值
+     */
+    public Map<String, Object> hmget(String key) {
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    /**
+     * HashSet
+     *
+     * @param key 键
+     * @param map 对应多个键值
+     * @return true 成功 false 失败
+     */
+    public boolean hmset(String key, Map<String, Object> map) {
+        try {
+            redisTemplate.opsForHash().putAll(key, map);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * HashSet 并设置时间
+     *
+     * @param key  键
+     * @param map  对应多个键值
+     * @param time 时间(秒)
+     * @return true成功 false失败
+     */
+    public boolean hmset(String key, Map<String, Object> map, long time) {
+        try {
+            redisTemplate.opsForHash().putAll(key, map);
+            if (time > 0) {
+                expire(key, time);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 向一张hash表中放入数据,如果不存在将创建
+     *
+     * @param key   键
+     * @param item  项
+     * @param value 值
+     * @return true 成功 false失败
+     */
+    public boolean hset(String key, String item, Object value) {
+        try {
+            redisTemplate.opsForHash().put(key, item, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 向一张hash表中放入数据,如果不存在将创建
+     *
+     * @param key   键
+     * @param item  项
+     * @param value 值
+     * @param time  时间(秒)  注意:如果已存在的hash表有时间,这里将会替换原有的时间
+     * @return true 成功 false失败
+     */
+    public boolean hset(String key, String item, Object value, long time) {
+        try {
+            redisTemplate.opsForHash().put(key, item, value);
+            if (time > 0) {
+                expire(key, time);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 删除hash表中的值
+     *
+     * @param key  键 不能为null
+     * @param item 项 可以使多个 不能为null
+     */
+    public void hdel(String key, Object... item) {
+        redisTemplate.opsForHash().delete(key, item);
+    }
+
+    /**
+     * 判断hash表中是否有该项的值
+     *
+     * @param key  键 不能为null
+     * @param item 项 不能为null
+     * @return true 存在 false不存在
+     */
+    public boolean hHasKey(String key, String item) {
+        return redisTemplate.opsForHash().hasKey(key, item);
+    }
+
+    /**
+     * hash递增 如果不存在,就会创建一个 并把新增后的值返回
+     *
+     * @param key  键
+     * @param item 项
+     * @param by   要增加几(大于0)
+     * @return
+     */
+    public double hincr(String key, String item, double by) {
+        return redisTemplate.opsForHash().increment(key, item, by);
+    }
+
+    /**
+     * hash递减
+     *
+     * @param key  键
+     * @param item 项
+     * @param by   要减少记(小于0)
+     * @return
+     */
+    public double hdecr(String key, String item, double by) {
+        return redisTemplate.opsForHash().increment(key, item, -by);
+    }
+
 }
--- common/src/main/java/com/cloud/common/util/HttpUtils.java
+++ common/src/main/java/com/cloud/common/util/HttpUtils.java
@@ -1,26 +1,152 @@
 package com.cloud.common.util;
 
 import com.google.common.base.Strings;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.*;
 import javax.servlet.http.HttpServletRequest;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * HttpUtils
  *
  * @author : 7le
- * @since v1.0.0
  */
 public class HttpUtils {
 
-    private final static Logger LOG = LoggerFactory.getLogger(HttpUtils.class);
+    private final static Logger log = LoggerFactory.getLogger(HttpUtils.class);
+    private static final int MAX_TIMEOUT = 3000;
+    private static final int SUCCESS = 200;
+    private static PoolingHttpClientConnectionManager connMgr;
+    private static RequestConfig requestConfig;
+
+    static {
+        // 设置连接池
+        connMgr = new PoolingHttpClientConnectionManager();
+        // 设置连接池大小
+        connMgr.setMaxTotal(100);
+        connMgr.setDefaultMaxPerRoute(connMgr.getMaxTotal());
+        requestConfig = RequestConfig.custom()
+                // 设置连接超时
+                .setConnectTimeout(MAX_TIMEOUT)
+                // 设置读取超时
+                .setSocketTimeout(MAX_TIMEOUT)
+                // 设置从连接池获取连接实例的超时
+                .setConnectionRequestTimeout(MAX_TIMEOUT).build();
+    }
+
+    private static CloseableHttpClient getHttpClient() throws NoSuchAlgorithmException {
+        return HttpClientBuilder.create().setMaxConnTotal(200)
+                .setMaxConnPerRoute(100)
+                .setSSLHostnameVerifier((String host, final SSLSession session) -> true)
+                .setSSLSocketFactory(new SSLConnectionSocketFactory(createSSLSocketFactory()))
+                .build();
+    }
+
+    private static SSLContext createSSLSocketFactory() {
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            }}, new SecureRandom());
+            return sc;
+        } catch (Exception e) {
+            log.warn("Exception:{}", e);
+        }
+        return null;
+    }
+
+    /**
+     * 不带参数的get请求
+     */
+    public static String get(String url) throws Exception {
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
+        try (CloseableHttpClient httpClient = getHttpClient();
+             CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            if (response.getStatusLine().getStatusCode() == SUCCESS) {
+                return EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 带参数的get请求
+     */
+    public static String get(String url, Map<String, String> params) throws Exception {
+        URIBuilder uriBuilder = new URIBuilder(url);
+        if (params != null) {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                uriBuilder.setParameter(entry.getKey(), entry.getValue());
+            }
+        }
+        return get(uriBuilder.build().toString());
+    }
+
+    /**
+     * 带参数的post请求
+     */
+    public static String post(String url, Map<String, String> params) throws Exception {
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setConfig(requestConfig);
+        if (params != null) {
+            List<NameValuePair> parameters = new ArrayList<>(0);
+            parameters.addAll(params.entrySet().stream()
+                    .map(entry -> new BasicNameValuePair(entry.getKey(), entry.getValue())).collect(Collectors.toList()));
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters);
+            httpPost.setEntity(formEntity);
+        }
+        try (CloseableHttpClient httpClient = getHttpClient();
+             CloseableHttpResponse response = httpClient.execute(httpPost)) {
+            if (response.getStatusLine().getStatusCode() == SUCCESS) {
+                return EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 不带参数的post请求
+     */
+    public static String post(String url) throws Exception {
+        return post(url, null);
+    }
 
     /**
      * 本机获取ip地址
@@ -54,32 +180,33 @@ public class HttpUtils {
      */
     public static String getClientIp(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("x-forwarded-for = {}", ip);
+        if (log.isDebugEnabled()) {
+            log.debug("x-forwarded-for = {}", ip);
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Proxy-Client-IP = {}", ip);
+            if (log.isDebugEnabled()) {
+                log.debug("Proxy-Client-IP = {}", ip);
             }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("WL-Proxy-Client-IP = {}", ip);
+            if (log.isDebugEnabled()) {
+                log.debug("WL-Proxy-Client-IP = {}", ip);
             }
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("RemoteAddr-IP = {}", ip);
+            if (log.isDebugEnabled()) {
+                log.debug("RemoteAddr-IP = {}", ip);
             }
         }
         if (!Strings.isNullOrEmpty(ip)) {
             ip = ip.split(":")[0];
         }
         String[] ipArray = ip.split(",");
-        for (String s : ipArray) {// 过滤掉局域网ip
+        // 过滤掉局域网ip
+        for (String s : ipArray) {
             s = s.trim();
             if (!(s.startsWith("10.") || s.startsWith("192."))) {
                 return s;
