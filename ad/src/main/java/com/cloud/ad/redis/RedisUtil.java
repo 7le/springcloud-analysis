64dcf58fb2296dd5388f57cf942c70c99f5c9340
7le <silk.heqian@gmail.com>
2018-07-22 22:43:25 +0800

    优化redis工具类 ⚽

--- ad/src/main/java/com/cloud/ad/redis/RedisUtil.java
+++ ad/src/main/java/com/cloud/ad/redis/RedisUtil.java
@@ -1,13 +1,23 @@
 package com.cloud.ad.redis;
 
+import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisCommands;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,7 +26,9 @@ import java.util.concurrent.TimeUnit;
  * @author 7le
  */
 @Component
-public class RedisUtil {
+public class RedisUtil extends AbstractDistributedLock{
+
+    private final Logger logger = LoggerFactory.getLogger(RedisUtil.class);
 
     @SuppressWarnings("rawtypes")
     @Autowired
@@ -286,4 +298,81 @@ public class RedisUtil {
         return redisTemplate.opsForHash().increment(key, item, -by);
     }
 
+    //============================Lock=============================
+
+    private ThreadLocal<String> lockFlag = new ThreadLocal<>();
+
+    private static final String UNLOCK_LUA;
+
+    static {
+        String str = "if redis.call(\"get\",KEYS[1]) == ARGV[1] " +
+                "then " +
+                "    return redis.call(\"del\",KEYS[1]) " +
+                "else " +
+                "    return 0 " +
+                "end ";
+        UNLOCK_LUA = str;
+    }
+
+    @Override
+    public <T> T lock(String key, long expire, int retryTimes, long sleepMillis, AcquiredLockWorker<T> worker) throws Exception{
+        boolean result = setRedis(key, expire);
+        // 如果获取锁失败，按照传入的重试次数进行重试
+        while((!result) && retryTimes-- > 0){
+            try {
+                logger.debug("lock failed, retrying..." + retryTimes);
+                Thread.sleep(sleepMillis);
+            } catch (InterruptedException e) {
+                throw new UnableToAcquireLockException("InterruptedException :",e);
+            }
+            result = setRedis(key, expire);
+        }
+        try {
+            return worker.invokeAfterLockAcquire();
+        } finally {
+            unLock(key);
+        }
+    }
+
+    private boolean setRedis(String key, long expire) {
+        try {
+            String result = (String) redisTemplate.execute((RedisCallback<String>) connection -> {
+                JedisCommands commands = (JedisCommands) connection.getNativeConnection();
+                String uuid = UUID.randomUUID().toString();
+                lockFlag.set(uuid);
+                return commands.set(key, uuid, "NX", "PX", expire);
+            });
+            return !Strings.isNullOrEmpty(result);
+        } catch (Exception e) {
+            logger.error("set redis occur an exception", e);
+        }
+        return false;
+    }
+
+    private boolean unLock(String key) {
+        // 释放锁的时候，有可能因为持锁之后方法执行时间大于锁的有效期，此时有可能已经被另外一个线程持有锁，所以不能直接删除
+        try {
+            List<String> keys = new ArrayList<>();
+            keys.add(key);
+            List<String> args = new ArrayList<>();
+            args.add(lockFlag.get());
+            // 使用lua脚本删除redis中匹配value的key，可以避免由于方法执行时间过长而redis锁自动过期失效的时候误删其他线程的锁
+            // spring自带的执行脚本方法中，集群模式直接抛出不支持执行脚本的异常，所以只能拿到原redis的connection来执行脚本
+            Long result = (Long) redisTemplate.execute((RedisCallback<Long>) connection -> {
+                Object nativeConnection = connection.getNativeConnection();
+                // 集群模式和单机模式虽然执行脚本的方法一样，但是没有共同的接口，所以只能分开执行
+                if (nativeConnection instanceof JedisCluster) {
+                    return (Long) ((JedisCluster) nativeConnection).eval(UNLOCK_LUA, keys, args);
+                }
+                else if (nativeConnection instanceof Jedis) {
+                    return (Long) ((Jedis) nativeConnection).eval(UNLOCK_LUA, keys, args);
+                }
+                return 0L;
+            });
+            return result != null && result > 0;
+        } catch (Exception e) {
+            logger.error("release lock occur an exception", e);
+        }
+        return false;
+    }
 }
